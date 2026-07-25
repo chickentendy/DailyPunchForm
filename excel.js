01e5a0f0319587bb75401ca77a4e48e79f3ee0b6
chickentendy <90145833+chickentendy@users.noreply.github.com>
2026-07-24 22:10:27 -0500

    Update excel.js

--- excel.js
+++ excel.js
@@ -1,6 +1,7 @@
 // ========================================
 // Field Work Tracker
 // Excel Export Module
+// Wage + Mileage Summary
 // ========================================
 
 
@@ -19,7 +20,7 @@ function exportExcel() {
 
 
 
-    if(weekEntries.length === 0) {
+    if(weekEntries.length === 0){
 
         alert("No entries found for this week.");
 
@@ -29,37 +30,20 @@ function exportExcel() {
 
 
 
-    let worksheetData = [];
-
-
-
-    // Title
-
-    worksheetData.push([
-        "Field Work Report"
-    ]);
-
-
-    worksheetData.push([
-        "Week",
-        selectedWeek
-    ]);
-
-
-    worksheetData.push([]);
-
-
-
-    // Summary
-
     let totalHours = 0;
     let totalMiles = 0;
     let totalAcres = 0;
     let totalTravel = 0;
 
+    let personalMiles = 0;
+    let mileageReimbursement = 0;
+
+    let hourlyRate = 0;
 
 
-    weekEntries.forEach(e => {
+
+    weekEntries.forEach(e=>{
+
 
         totalHours += e.hours;
 
@@ -69,44 +53,194 @@ function exportExcel() {
 
         totalTravel += e.travel;
 
+
+
+        if(e.vehicle === "Personal Vehicle"){
+
+            personalMiles += e.miles;
+
+            mileageReimbursement +=
+                e.miles * e.mileageRate;
+
+        }
+
+
+        if(e.hourlyRate > 0){
+
+            hourlyRate = e.hourlyRate;
+
+        }
+
+
     });
 
 
 
-    worksheetData.push([
-        "Weekly Totals"
+    let regularHours =
+        Math.min(totalHours,40);
+
+
+    let overtimeHours =
+        Math.max(totalHours-40,0);
+
+
+
+    let regularPay =
+        regularHours * hourlyRate;
+
+
+
+    let overtimePay =
+        overtimeHours *
+        (hourlyRate * 1.5);
+
+
+
+    let totalWages =
+        regularPay + overtimePay;
+
+
+
+    let totalCompensation =
+        totalWages +
+        mileageReimbursement;
+
+
+
+
+    let data = [];
+
+
+
+    // ---------- Header ----------
+
+
+    data.push([
+        "FIELD WORK REPORT"
     ]);
 
-    worksheetData.push([
-        "Hours Worked",
+
+    data.push([
+        "Week Starting Monday",
+        selectedWeek
+    ]);
+
+
+    data.push([]);
+
+
+
+    // ---------- Summary ----------
+
+
+    data.push([
+        "WEEKLY SUMMARY"
+    ]);
+
+
+
+    data.push([
+        "Total Hours",
         totalHours
     ]);
 
-    worksheetData.push([
-        "Miles Driven",
-        totalMiles
+
+    data.push([
+        "Regular Hours",
+        regularHours
     ]);
 
-    worksheetData.push([
-        "Acres Sampled",
-        totalAcres
-    ]);
 
-    worksheetData.push([
-        "Travel Time",
-        totalTravel
+    data.push([
+        "Overtime Hours",
+        overtimeHours
     ]);
 
 
 
-    worksheetData.push([]);
+    data.push([]);
 
 
 
+    data.push([
+        "HOURLY WAGES"
+    ]);
 
-    // Detailed Table
 
-    worksheetData.push([
+    data.push([
+        "Hourly Rate",
+        `$${hourlyRate.toFixed(2)}`
+    ]);
+
+
+    data.push([
+        "Regular Pay",
+        `$${regularPay.toFixed(2)}`
+    ]);
+
+
+    data.push([
+        "Overtime Pay",
+        `$${overtimePay.toFixed(2)}`
+    ]);
+
+
+    data.push([
+        "Total Wages",
+        `$${totalWages.toFixed(2)}`
+    ]);
+
+
+
+    data.push([]);
+
+
+
+    data.push([
+        "MILEAGE REIMBURSEMENT"
+    ]);
+
+
+    data.push([
+        "Personal Vehicle Miles",
+        personalMiles
+    ]);
+
+
+    data.push([
+        "Mileage Rate",
+        `$${weekEntries[0]?.mileageRate.toFixed(2)}`
+    ]);
+
+
+    data.push([
+        "Mileage Reimbursement",
+        `$${mileageReimbursement.toFixed(2)}`
+    ]);
+
+
+
+    data.push([]);
+
+
+
+    data.push([
+        "TOTAL COMPENSATION",
+        `$${totalCompensation.toFixed(2)}`
+    ]);
+
+
+
+    data.push([]);
+
+    data.push([]);
+
+
+
+    // ---------- Detailed Entries ----------
+
+
+    data.push([
 
         "Date",
 
@@ -124,7 +258,11 @@ function exportExcel() {
 
         "Travel Time",
 
-        "Location Area",
+        "Vehicle",
+
+        "Mileage Rate",
+
+        "Location",
 
         "Comments"
 
@@ -133,10 +271,11 @@ function exportExcel() {
 
 
 
-    weekEntries.forEach(e => {
+    weekEntries.forEach(e=>{
 
 
-        worksheetData.push([
+        data.push([
+
 
             e.date,
 
@@ -154,9 +293,14 @@ function exportExcel() {
 
             e.travel,
 
+            e.vehicle,
+
+            `$${e.mileageRate.toFixed(2)}`,
+
             e.location,
 
             e.comments
+
 
         ]);
 
@@ -167,31 +311,39 @@ function exportExcel() {
 
 
 
-    // Create Excel Sheet
-
     let worksheet =
-        XLSX.utils.aoa_to_sheet(
-            worksheetData
-        );
+        XLSX.utils.aoa_to_sheet(data);
 
 
-
-    // Column sizing
 
     worksheet["!cols"] = [
 
+        {width:18},
+
+        {width:12},
+
+        {width:18},
+
+        {width:18},
+
+        {width:12},
+
+        {width:12},
+
+        {width:18},
+
         {width:15},
-        {width:12},
+
         {width:18},
-        {width:18},
-        {width:12},
-        {width:12},
-        {width:18},
+
         {width:15},
+
         {width:20},
-        {width:40}
+
+        {width:45}
 
     ];
+
 
 
 
@@ -213,18 +365,11 @@ function exportExcel() {
 
 
 
-    // File Name
-
-    let filename =
-        `Field_Work_Report_${selectedWeek}.xlsx`;
-
-
-
     XLSX.writeFile(
 
         workbook,
 
-        filename
+        `Field_Work_Report_${selectedWeek}.xlsx`
 
     );
 
