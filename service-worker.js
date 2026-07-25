b06c5cb8af0d25fab9d39a99b0f5b114a3d6373f
chickentendy <90145833+chickentendy@users.noreply.github.com>
2026-07-24 21:30:26 -0500

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,139 @@
+// ========================================
+// Field Work Tracker
+// Service Worker
+// Offline Support
+// ========================================
+
+
+const CACHE_NAME = "field-work-tracker-v1";
+
+
+const FILES_TO_CACHE = [
+
+    "./",
+
+    "./index.html",
+
+    "./style.css",
+
+    "./script.js",
+
+    "./excel.js",
+
+    "./manifest.json"
+
+];
+
+
+
+
+// Install Service Worker
+
+self.addEventListener(
+    "install",
+    event => {
+
+
+        event.waitUntil(
+
+            caches.open(CACHE_NAME)
+
+            .then(cache => {
+
+                return cache.addAll(
+                    FILES_TO_CACHE
+                );
+
+            })
+
+        );
+
+
+        self.skipWaiting();
+
+
+    }
+
+);
+
+
+
+
+// Activate Service Worker
+
+self.addEventListener(
+    "activate",
+    event => {
+
+
+        event.waitUntil(
+
+            caches.keys()
+
+            .then(keys => {
+
+
+                return Promise.all(
+
+                    keys.map(key => {
+
+
+                        if(
+                            key !== CACHE_NAME
+                        ){
+
+                            return caches.delete(key);
+
+                        }
+
+
+                    })
+
+                );
+
+
+            })
+
+        );
+
+
+        self.clients.claim();
+
+
+    }
+
+);
+
+
+
+
+
+// Fetch Files
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+
+        event.respondWith(
+
+            caches.match(
+                event.request
+            )
+
+            .then(response => {
+
+
+                return response || fetch(
+                    event.request
+                );
+
+
+            })
+
+        );
+
+
+    }
+
+);
